694b59d1fedbd9fda3c9210e8655e0b0c5001395
Harlan Howe <harlan.howe@kinkaid.org>
2025-01-22 20:16:09 -0600

    Upgraded to Java 22 (I hope).

--- src/SliderPuzzleRunner.java
+++ src/SliderPuzzleRunner.java
@@ -1,7 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class SliderPuzzleRunner
 {
-    public static void main(String[] args)
+    public void main()
     {
         System.out.println("Start the program!");
+
+        for (int r = 1; r <= 4; r++)
+            for (int c = 1; c <= 4; c++)
+            {
+                ImageIcon tempIcon = new ImageIcon("SealParts/Kinkaid Seal-0" + r + "-0" + c + ".png");
+                Image imageToUse = tempIcon.getImage();
+                // store imageToUse in a long term variable.
+            }
     }
 }
